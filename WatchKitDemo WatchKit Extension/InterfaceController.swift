099433a4c3ce63f5073967bb7b6c26e748c14933
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-03-07 12:26:11 +1100

    Tidy up

--- WatchKitDemo WatchKit Extension/InterfaceController.swift
+++ WatchKitDemo WatchKit Extension/InterfaceController.swift
@@ -21,20 +21,20 @@ class InterfaceController: WKInterfaceController {
 
   override func awakeWithContext(context: AnyObject?) {
     super.awakeWithContext(context)
-    
+
     // Configure interface objects here.
   }
 
   override func willActivate() {
     // This method is called when watch view controller is about to be visible to user
-      super.willActivate()
+    super.willActivate()
 
     showImages()
   }
 
   override func didDeactivate() {
-      // This method is called when watch view controller is no longer visible
-      super.didDeactivate()
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
   }
 
   @IBAction func onQuaButtonTapped() {
